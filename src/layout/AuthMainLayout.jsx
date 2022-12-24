737965d1f0776a5ec46807413e0423c61717c50b
Siddharth <officialsiddharthbisht@gmail.com>
2022-12-24 10:37:45 +0530

    ADD:Layout Setup

--- src/layout/AuthMainLayout.jsx
+++ src/layout/AuthMainLayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 function AuthMainLayout() {
     return (
-        <div>AuthMainLayout</div>
+        <div>
+            <Outlet />
+        </div>
     )
 }
 
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../Components/Navbar';
+import { Outlet } from 'react-router-dom';
 function MainLayout() {
     return (
         <div>
             <Header />
+            <Outlet />
         </div>
     )
 }
